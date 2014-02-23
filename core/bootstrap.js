dcc8f20d2e99a1c4e01e16214bdb8bceacbebb34
Chiel Kunkels <chiel@kunkels.me>
2014-02-23 17:06:32 +0100

    Add ability to load modules from additional paths

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -1,3 +1,14 @@
 require('./app');
 require('./config');
-require('./modules').load(__dirname + '/../modules');
+
+var isArray = require('mout/lang/isArray'),
+	modules = require('./modules'),
+	config = require('../lib/parseJSONFile')(__dirname + '/../config.json');
+
+modules.load(__dirname + '/modules');
+
+if (config.modulePaths && isArray(config.modulePaths)){
+	for (var i = 0, len = config.modulePaths.length; i < len; i++){
+		modules.load(__dirname + '/../' + config.modulePaths[i]);
+	}
+}
